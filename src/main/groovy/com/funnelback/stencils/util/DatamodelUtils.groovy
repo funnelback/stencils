9c6e05826060e8d769883800e29a72838d291a48
nguillaumin <nguillaumin@funnelback.com>
2017-04-26 11:01:15 -0700

    Avoid a NullPointerException if the query string map is missing
    This will happen with extra searches, as they run in a different thread
    we don't have access to the original request to get the query string
    from. This is actually common when using the Tab Stencil as it uses an
    extra search.
    
    When we don't have a source map to copy from, just return an empty map.
    That will allow the hooks to work, albeit in degraded mode where the
    generated URLs will miss some parameters.

--- src/main/groovy/com/funnelback/stencils/util/DatamodelUtils.groovy
+++ src/main/groovy/com/funnelback/stencils/util/DatamodelUtils.groovy
@@ -20,8 +20,16 @@ class DatamodelUtils {
      * @return Copied map
      */
     public static Map getQueryStringMapCopy(Map queryStringMap) {
-        return queryStringMap.collectEntries {
-            e -> [e.key, new ArrayList(e.value)]
+        if (queryStringMap == null) {
+            // If we don't have a source map to copy, just return an empty
+            // map. This may happen within extra searches, and it will still allow the
+            // hook to works without crashing. The resulting URL may miss some parameters
+            // the they can be re-added in the template if needed
+            return [:]
+        } else {
+            return queryStringMap.collectEntries {
+                e -> [e.key, new ArrayList(e.value)]
+            }
         }
     }
 
