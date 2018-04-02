1aca87a128969194ae9a6fcd46a677570979d856
Nicolas Guillaumin <nguillaumin@funnelback.com>
2018-04-02 09:54:19 -0700

    Updated partializing logic following code review comments
    Instead of just creating separate triggers for each individual words,
    respect the original sentence that was in the CSV. For example with
    "What is chocolate powder", generate:
    
    * What is chocolate powder
    * chocolate powder
    * powder
    
    The first one just copies the phrase as-is so that it will match a user
    searching for "What is...". The following ones generate sub-sentences by
    removing the first word every time (and stripping leading stop words)

--- src/main/groovy/com/funnelback/stencils/workflow/autocompletion/CSVAutoCompletionsPartializer.groovy
+++ src/main/groovy/com/funnelback/stencils/workflow/autocompletion/CSVAutoCompletionsPartializer.groovy
@@ -37,22 +37,39 @@ class CSVAutoCompletionsPartializer {
                     if (triggerWords.trim().split("\\s").length > 1) {
                         // If the trigger words is a phrase, insert the original
                         // record as-is so that it will match the phrase.
-                        // For example "Chocolate cake" should match the partial
-                        // query "chocolate ca". If we were not copying the record
-                        // as is it would only match "chocolate" or "cake"
+                        // For example "What is chocolate" should match the partial
+                        // query "what is cho". If we were not copying the record
+                        // as is it would only match "chocolate" because the leading
+                        // stop words would have been eliminated ("what" and "is")
                         printer.printRecord(record)
                     }
 
                     // Split our words on space
-                    triggerWords.trim().split("\\s")
+                    def triggerList = triggerWords.trim().split("\\s")
                     // Canonicalize the trigger: Remove punctuation, convert to lower case
                     .collect() { it.trim().replaceAll("\\W", "").toLowerCase() }
                     // Ignore empty triggers (e.g. consecutive spaces or triggers containing only punctuation)
                     .findAll() { it != "" }
-                    // Ensure it's not a stop word
-                    .findAll() { ! stopWords.contains(it) }
-                    .each { trigger ->
-                        def newRecord = [ trigger ]
+
+                    // At this point we have a list of words like "what is chocolate powder"
+                    // We need to generate new records with:
+                    // - what is chocolate powder
+                    // - is chocolate powder
+                    // - chocolate powder
+                    // - chocolate
+                    // .. while eliminating the stopwords, so in practice:
+                    // - chocolate powder
+                    // - chocolate
+
+                    for (def j=0; j< triggerList.size(); j++) {
+                        def word = triggerList[j]
+
+                        // Skip leading stopwords
+                        if (stopWords.contains(word)) {
+                            continue
+                        }
+
+                        def newRecord = [ triggerList.subList(j, triggerList.size()).join(" ") ]
 
                         // Generate a row in the output CSV file by copying
                         // the rest of the column from the input CSV
@@ -62,6 +79,7 @@ class CSVAutoCompletionsPartializer {
 
                         printer.printRecord(newRecord)
                         generated++
+
                     }
 
                     processed++
